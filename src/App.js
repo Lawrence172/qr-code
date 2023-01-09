96f3f8bfe22979e1ae2bc70adf539c5563a64886
Olatunde Lawrence <93929624+Lawrence172@users.noreply.github.com>
2023-01-09 12:28:41 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import QRCode from "react-qr-code";
+import { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState("")
+  
+  function handleChange(e){
+    setText(e.target.value)
+  }
+
   return (
     <div className='App'>
       <h1>Qr Code Generator</h1>
-      <QRCode value="tutorend.com" />
+      <QRCode value={text} />
+
+      <div className='input-here'>
+        <p>Enter your text here</p>
+        <input type="text" value={text} onChange={(e)=>{handleChange(e)}} />
+      </div>
+
     </div>
   );
 }
